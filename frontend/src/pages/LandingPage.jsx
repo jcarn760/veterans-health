a878503da36a59c2c0ca3dab9f0959897126dbe7
jcarn760 <joshuacarnahan@yahoo.com>
2024-02-18 12:24:06 -0500

    modify styling using prettier. update home page and landing page to a tabbed out partially functioning frontend.

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -3,12 +3,13 @@ import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
 import { NavLink as RouterLink, useNavigate } from "react-router-dom";
 
 export const LandingPage = () => {
   return (
     <>
-      <Box pl="19%" pr="3%">
+      <Box pl="5%" pr="5%">
         <Stack direction="row" spacing="20px">
           <img
             width="50%"
@@ -17,13 +18,13 @@ export const LandingPage = () => {
             style={{ borderRadius: "90px" }}
           />
           <Stack>
-            <h1>Welcome to Veteran Health</h1>
-            <h3>
+            <Typography variant="h3">Welcome to Veteran Health</Typography>
+            <Typography variant="h6">
               Veteran health is a one stop shop for tracking your fitness goals
               and providing resources for your mental well being.
-            </h3>
-            <Divider />
-            <h3>Get started by registering or logging in below.</h3>
+            </Typography>
+            <Divider style={{ paddingTop: '5px', paddingBottom: '5px' }} />
+            <Typography paddingTop="5px" variant="h6">Get started by registering or logging in below.</Typography>
             <Stack direction="row" pt="12%" pl="33%" spacing="10px">
               <RouterLink to="/login">
                 <Button variant="contained">Login</Button>
